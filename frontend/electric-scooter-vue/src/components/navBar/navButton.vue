3187255aa3af754071e85a1632eae1428556c694
Romello <romello.ten.broeke@hva.nl>
2023-09-08 15:44:07 +0200

    Started creating navBar

---
+++ frontend/electric-scooter-vue/src/components/navBar/navButton.vue
@@ -0,0 +1,16 @@
+<template>
+<li>{{ navigationItem }}</li>
+</template>
+
+<script>
+export default {
+  name: "navButton",
+  props: {
+    navigationItem: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
