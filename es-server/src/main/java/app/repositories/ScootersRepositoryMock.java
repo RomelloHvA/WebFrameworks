99131bf7d985573b791ba21f5f99e6532908e5e7
Romello <romello.ten.broeke@hva.nl>
2023-10-09 14:16:42 +0200

    3.6A finished

--- es-server/src/main/java/app/repositories/ScootersRepositoryMock.java
+++ es-server/src/main/java/app/repositories/ScootersRepositoryMock.java
@@ -17,31 +17,47 @@ public class ScootersRepositoryMock implements ScootersRepository{
         scooters = new ArrayList<>();
 
         for (int i = 0; i < numberOfScooters; i++) {
-            if (!idExists(startId + i)) {
+            if (findById(startId+i) == null) {
                 scooters.add(Scooter.createSampleScooter(startId+i));
             }
         }
     }
 
-    /**
-     * Method for finding a given Id. This method should be implemented whenever a scooter is added to the list.
-     * @param idToFind is the Id to find before being able to add to the scooterslist
-     * @return if the id exists.
-     * @author Romello ten Broeke
-     */
-    public boolean idExists(int idToFind){
-        for (Scooter scooter : scooters){
-            if (scooter.getId() == idToFind) {
-                return true;
-            }
-        }
-        return false;
-    }
 
 
     @Override
     public List findAll() {
         return scooters;
+    }
+    /**
+     * Method for finding a given Id. This method should be implemented whenever a scooter is added to the list.
+     * @param id is the Id to find before being able to add to the scooterslist
+     * @return if the id exists.
+     * @author Romello ten Broeke
+     */
+    @Override
+    public Scooter findById(long id) {
+        for (Scooter scooter : scooters){
+            if (scooter.getId() == id){
+                return scooter;
+            }
+        }
+        //No scooter exists by this id
+        return null;
+    }
+
+    @Override
+    public Scooter save(Scooter scooter) {
+        return null;
+    }
+
+    @Override
+    public Scooter deleteById(long id) {
+        if (findById(id) != null){
+            scooters.remove(findById(id));
+        }
+        // Cant find the id
+        return null;
     }
 
 }
